a59ad2bcdc99280d2cb941ad72ba6ccc0181de00
GustaBarbosaa <gubarbosa038@gmail.com>
2024-04-26 20:04:15 -0300

    Initial commit

---
+++ src/components/ListPersonagens.vue
@@ -0,0 +1,28 @@
+<script setup>
+    const personagem = defineProps(["name", "url", "status", "species", "gender", "location", "image", "episode"])
+
+</script>
+
+<template>
+    <div class="col-md-3">
+        <div class="card mb-3 p-3">
+            <img :src="personagem.image" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-title"><b>Nome:</b> {{ personagem.name }}</p>
+                <p class="card-title"><b>Status: </b>
+                <font style="background-color: green;" v-if="personagem.status === 'Alive'" color="white">{{ personagem.status }}</font>
+                <font style="background-color: yellow;" v-else-if="personagem.status === 'unknown' "color="white">{{ personagem.status }}</font>
+                <font style="background-color: red;"v-else color="white">{{ personagem.status }}</font></p>
+                <p class="card-title"><b>Espécie:</b> {{ personagem.species }}</p>
+                <p class="card-title"><b>Gênero:</b> {{ personagem.gender }}</p>
+                <p class="card-title"><b>Localização Atual:</b> {{ personagem.location.name }}</p>
+                <p class="card-title"><b>Episódios:</b> {{ personagem.episode.length }}</p>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home" class="container-fluid">
+     <div class="row justify-content-center">
+             <img class="col-md-3 card mb-4 p-4" src="C:\Users\gubar\OneDrive\trabalhoP2_Valdir\src\assets\foto.jpg" alt="">
+              <div class="justify-content-center text-center">
+         </div>
+         <div class="col-lg-3">
+                 <div class="d-flex justify-content-center">
+                    <div>
+                     <p><b>NOME:</b> Gustavo Barbosa da Silva</p>
+                      <p><b>IDADE:</b> 19 anos</p>
+                      <p><b>UNIVERSIDADE:</b> UNIMAR</p>
+                      <p><b>ANO/PERIODO:</b> 3ªA Noturno</p>
+                      <p><b>RA:</b> 1961559</p>
+                    </div>
+                 </div>
+         </div>
+     </div>
+  </div>
+</template>
+
